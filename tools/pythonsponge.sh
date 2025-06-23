8b690756f458dc6c57a180177e5fee94fea9e66d
gdenes <gdenes355@gmail.com>
2025-06-23 09:25:27 +0200

    fix path

--- tools/pythonsponge.sh
+++ tools/pythonsponge.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -L "$SOURCE" ]; do
+  DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+SCRIPT_DIR="$(cd -P "$(dirname "$SOURCE")" >/dev/null 2>&1 && pwd)"
 
 print_help() {
     echo "Usage: $0 [command]"

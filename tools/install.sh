5cf9d78ba167ba82d1e31d792a42240d67e00c82
Gyorgy Denes <gdenes355@gmail.com>
2025-12-31 18:55:26 +0100

    fix

--- tools/install.sh
+++ tools/install.sh
@@ -149,7 +149,7 @@ else
     git -C "\$DEPLOYED_DIR/server" pull
 fi
 
-echi "Setting permissions for restart-ws.sh to be owned by sudo, but executable by anyone"
+echo "Setting permissions for restart-ws.sh to be owned by sudo, but executable by anyone"
 sudo chmod 755 /home/$SERVICE_USER/deployed/server/tools/restart-ws.sh
 sudo chown root:root /home/$SERVICE_USER/deployed/server/tools/restart-ws.sh
 
